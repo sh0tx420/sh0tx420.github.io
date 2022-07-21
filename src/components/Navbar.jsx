8bfe35242448d5b4f5342c5884925ff2590fac30
sh0tx420 <sh0tx.cosmoline@gmail.com>
2022-07-21 19:00:03 +0300

    Optimized CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 export default function Navbar() {
     return (
-        <nav class="flex py-3 bg-gray-900">
+        <nav class="flex py-3 bg-gray-900 mb-2">
             <div class="px-6">
                 <div>
-                    <a class="text-xl font-bold text-white" href="#">sh0tx420.github.io</a>
-                    <button class="ml-3 h-10 bg-red-900 rounded-lg p-2">
-                        <a class="text-white" href="/">Home</a>
+                    <a class="text-xl font-bold" href="#">sh0tx420.github.io</a>
+                    <button class="ml-3 h-10 bg-blue-700 hover:bg-blue-600 rounded-lg p-2">
+                        <a href="/">Home</a>
                     </button>
-                    <button class="ml-3 h-10 bg-red-900 rounded-lg p-2">
-                        <a class="text-white" href="/blog">Blog</a>
+                    <button class="ml-3 h-10 bg-blue-700 hover:bg-blue-600 rounded-lg p-2">
+                        <a href="/blog">Blog</a>
                     </button>
                 </div>
             </div>
