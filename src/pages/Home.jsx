69dddc003632f804a949418a8b2ec9e0e5f8b266
sh0tx420 <sh0tx.cosmoline@gmail.com>
2022-07-25 22:03:18 +0300

    Added projects to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,9 +17,9 @@ export default function Home() {
         "vue"
     ];
 
-    return (
+    return <>
         <div class="flex justify-center">
-            <div class="flex grid gap-4 grid-cols-3 grid-rows-3">
+            <div class="flex grid gap-4 grid-cols-2">
                 <div class="bg-gray-900 rounded-lg p-2">
                     <h1 class="text-center font-bold mb-2">Languages & frameworks</h1>
                     <For each={langs}>
@@ -33,7 +33,28 @@ export default function Home() {
                         {framework => <img src={`/src/assets/logos/logo_${framework}.png`} width={img_size} height={img_size} class="float-left mr-1" />}
                     </For>
                 </div>
+                <div class="bg-gray-900 rounded-lg p-2">
+                    <h1 class="text-center font-bold mb-2">About</h1>
+                    <div class="text-center">
+                        i do programming :D
+                    </div>
+                </div>
             </div>
         </div>
-    );
+        <div class="flex justify-center">
+            <div class="mt-5 w-1/3 h-screen">
+                <div class="bg-gray-900 rounded-lg p-2">
+                    <h1 class="text-center font-bold mb-2">Projects</h1>
+                    <div class="flex grid gap-4 grid-rows-1">
+                        <div class="flex justify-center">
+                            <a class="bg-blue-900 hover:bg-blue-800 rounded-lg w-full p-2" href="https://bn-servers.cf">
+                                <h1 class="font-bold text-lg">Boundless Networks</h1>
+                                A network of game servers for Source-based games.
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>;
 }
