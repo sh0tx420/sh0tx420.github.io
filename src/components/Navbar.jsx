9eb2ca2f46e27801ffbbf28aa4bda56792ad7aab
sh0tx420 <sh0tx.cosmoline@gmail.com>
2022-08-02 14:22:23 +0300

    Improved navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,15 @@
 export default function Navbar() {
+
     return (
         <nav class="flex py-3 bg-gray-900 mb-2">
-            <div class="px-6">
-                <div>
-                    <a class="text-xl font-bold" href="#">sh0tx420.github.io</a>
-                    <button class="ml-3 h-10 bg-blue-700 hover:bg-blue-600 rounded-lg p-2">
-                        <a href="/">Home</a>
-                    </button>
-                    <button class="ml-3 h-10 bg-blue-700 hover:bg-blue-600 rounded-lg p-2">
-                        <a href="/blog">Blog</a>
-                    </button>
-                </div>
+            <a class="ml-4 font-bold text-2xl" href="#">sh0tx420.github.io</a>
+            <div class="flex grid grid-cols-2 gap-4 px-6">
+                <a class="bg-blue-700 hover:bg-blue-600 rounded-lg p-2 text-center" href="/">
+                    Home
+                </a>
+                <a class="bg-blue-700 hover:bg-blue-600 rounded-lg p-2 text-center" href="/blog">
+                    Blog
+                </a>
             </div>
         </nav>
     );
