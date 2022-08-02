1f8005964d12239e5af4a73263c157da4d56ddb7
sh0tx420 <sh0tx.cosmoline@gmail.com>
2022-08-02 14:23:05 +0300

    Updated home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,14 +42,20 @@ export default function Home() {
             </div>
         </div>
         <div class="flex justify-center">
-            <div class="mt-5 w-1/3 h-screen">
+            <div class="mt-5 w-1/3">
                 <div class="bg-gray-900 rounded-lg p-2">
                     <h1 class="text-center font-bold mb-2">Projects</h1>
-                    <div class="flex grid gap-4 grid-rows-1">
+                    <div class="flex grid gap-4 grid-rows-2">
                         <div class="flex justify-center">
                             <a class="bg-blue-900 hover:bg-blue-800 rounded-lg w-full p-2" href="https://bn-servers.cf">
                                 <h1 class="font-bold text-lg">Boundless Networks</h1>
                                 A network of game servers for Source-based games.
+                            </a>
+                        </div>
+                        <div class="flex justify-center">
+                            <a class="bg-blue-900 hover:bg-blue-800 rounded-lg w-full p-2" href="https://nanoarch.net">
+                                <h1 class="font-bold text-lg">NanoArch</h1>
+                                Fast and powerful all-purpose Discord bot.
                             </a>
                         </div>
                     </div>
