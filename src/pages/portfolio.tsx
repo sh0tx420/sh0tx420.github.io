bce517d837c2bb7c76bed29f8401dc2b2fbcf478
sh0tx420 <55631215+sh0tx420@users.noreply.github.com>
2024-08-20 15:32:06 +0300

    fix: Remove placeholder text

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -3,8 +3,6 @@ import MDFile from "@/components/mdfile";
 export default function Portfolio() {
     return (
         <div>
-            helo привет здарова
-
             <div className="flex mt-10 max-w-fit mx-auto p-3 mb-[40vh] bg-neutral-800 rounded-md shadow-lg">
                 <div className="p-2" id="mdfile">
                     <MDFile />
